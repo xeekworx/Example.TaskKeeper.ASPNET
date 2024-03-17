03eb83293d9ea72945beaa09b9e6d337b67e4d2c
Xeek <xeek@xeekworx.com>
2024-03-17 14:13:33 -0500

    chore: adding an integration test for get TaskItems

---
+++ TaskKeeper.IntegrationTests/SearchTaskItemTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace TaskKeeper.IntegrationTests
+{
+    public class SearchTaskItemTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public SearchTaskItemTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_TaskItems_ReturnOk()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/TaskItem");
+
+            // Assert 
+            response.IsSuccessStatusCode.Should().BeTrue();
+        }
+    }
+}
